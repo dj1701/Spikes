55cdecae4ff7d4d76e61f749756a8784f91384f8
David Jacobs <david.jacobs@bglgroup.co.uk>
2014-07-02 16:32:49 +0100

    DJ: Update to have separate console apps for publish and subscribe.

---
+++ RabbitMQ/PublishSubscribe/Spike.Subscribe/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace Spike.Subscribe
+{
+    class Program
+    {
+        public interface ISubscribe
+        {
+            string ReceiveFromFanOut(); 
+            string ReceiveFromDirect();
+        }
+
+        public class MessageSubscribe : ISubscribe
+        {
+            private readonly ConnectionFactory _factory = new ConnectionFactory();
+            private const string _fanoutExchange = "FanOut-Exchange";
+            private const string _directExchange = "Direct-Exchange";
+            private const string _fanoutQueue = "QueueForFanOutExchange";
+            private const string _directQueue = "QueueForDirectExchange";
+
+            public MessageSubscribe()
+            {
+                _factory.Uri = "amqp://localhost";
+                _factory.Protocol = Protocols.DefaultProtocol;
+                _factory.Port = AmqpTcpEndpoint.UseDefaultPort;
+            }
+            
+            public string ReceiveFromDirect()
+            {
+                string receivedMessage;
+
+                using (IConnection conn = _factory.CreateConnection())
+                using (IModel channel = conn.CreateModel())
+                {
+                    channel.QueueDeclare(_directQueue, true, false, false, null);
+                    channel.ExchangeDeclare(_directExchange, "direct");
+                    channel.QueueBind(_directQueue, _fanoutExchange, "");
+
+                    var receiveEncodedMessage = channel.BasicGet(_directQueue, false);
+                    receivedMessage = System.Text.Encoding.Unicode.GetString(receiveEncodedMessage.Body);
+                }
+
+                return receivedMessage;
+            }
+
+            public string ReceiveFromFanOut()
+            {
+                string message;
+
+                using (IConnection conn = _factory.CreateConnection())
+                using (IModel channel = conn.CreateModel())
+                {
+                    channel.QueueDeclare(_fanoutQueue, true, false, false, null);
+                    channel.ExchangeDeclare(_directExchange, "fanout");
+                    channel.QueueBind(_fanoutQueue, _directExchange, "");
+
+                    var encodedMessage = channel.BasicGet(_fanoutQueue, false);
+                    message = System.Text.Encoding.Unicode.GetString(encodedMessage.Body);
+                }
+
+                return message;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var subscribe = new MessageSubscribe();
+
+            Console.WriteLine("");
+            Console.WriteLine("Main Menu (Receive Messages)");
+            Console.WriteLine("==========================");
+            Console.WriteLine("1. Receive from Fan Out Exchange");
+            Console.WriteLine("2. Send Direct Exchange Message");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine("Enter your choice: ");
+            var input = Console.ReadLine();
+            int choice = 0;
+
+            if (!int.TryParse(input, out choice)) return;
+            switch (choice)
+            {
+                case 1:
+                    subscribe.ReceiveFromFanOut();
+                    var receiveFromFanOut = subscribe.ReceiveFromFanOut();
+                    Console.Write("Message received from Fan Out Exchange: \n\n{0}",receiveFromFanOut);
+                    Console.Read();
+                    break;
+                case 2:
+                    var receiveFromDirect = subscribe.ReceiveFromDirect();
+                    Console.Write("Message received from Direct Exchange: \n\n{0}",receiveFromDirect);
+                    Console.Read();
+                    break;
+                default:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+}
--- RabbitMQ/PublishSubscribe/Src/Spike.PublishSubscribe/Program.cs
+++ RabbitMQ/PublishSubscribe/Src/Spike.PublishSubscribe/Program.cs
@@ -1,17 +1,25 @@
 ﻿using System;
+using System.Net.Sockets;
 using RabbitMQ.Client;
 
-namespace Spike.PublishSubscribe
+namespace Spike.Publish
 {
-    public class RabbitMQ
+    public interface IPublish
+    {
+        void SendToFanOutExchange();
+        void SendToDirectExchange();
+    }
+
+    public class MessagingPublish : IPublish
     {
         private readonly ConnectionFactory _factory = new ConnectionFactory();
-        private const string _q1 = "q1";
-        private const string _q2 = "q2";
-        private const string _exchange = "Exchange";
-        private const string _routingKey = "green";
-        
-        public RabbitMQ()
+        private const string _routingKey = "123";
+        private const string _fanoutExchange = "FanOut-Exchange";
+        private const string _directExchange = "Direct-Exchange";
+        private const string _fanoutQueue = "QueueForFanOutExchange";
+        private const string _directQueue = "QueueForDirectExchange";
+
+        public MessagingPublish()
         {
             //_factory.Uri = "amqp://user:pass@hostName:port/vhost";
             _factory.Uri = "amqp://localhost";
@@ -19,95 +27,70 @@ namespace Spike.PublishSubscribe
             _factory.Port = AmqpTcpEndpoint.UseDefaultPort;
         }
 
-        public void SendToFanOut() 
+        public void SendToFanOutExchange() 
         {
             using (IConnection conn = _factory.CreateConnection())
             using (IModel channel = conn.CreateModel())
             {
-                channel.QueueDeclare("TestQueue", true,false,false,null);
-                channel.ExchangeDeclare("TestExchange", "fanout");
-                channel.QueueBind("TestQueue", "TestExchange", "");
+                channel.QueueDeclare(_fanoutQueue, true,false,false,null);
+                channel.ExchangeDeclare(_fanoutExchange, "fanout");
+                channel.QueueBind(_fanoutQueue, _fanoutExchange, "");
 
-                const string message = "Test Message";
+                const string message = "Hello Motor Team!!  This message is sent through a fanout exchange.";
                 var encodedMessage = System.Text.Encoding.Unicode.GetBytes(message);
-                channel.BasicPublish("TestExchange", "", null, encodedMessage);
+                channel.BasicPublish(_fanoutExchange, "", null, encodedMessage);
             }
         }
 
-        public string ReceiveFromFanOut()
-        {
-            string message;
-
-            using (IConnection conn = _factory.CreateConnection())
-            using (IModel channel = conn.CreateModel())
-            {
-                //channel.QueueDeclare("TestQueue", true, false, false, null);
-                //channel.ExchangeDeclare("TestExchange", "fanout");
-                //channel.QueueBind("TestQueue", "TestExchange", "");
-
-                var encodedMessage = channel.BasicGet("TestQueue", false);
-                message = System.Text.Encoding.Unicode.GetString(encodedMessage.Body);
-            }
-
-            return message;
-        }
-
-        public void SendToDirect()
+        public void SendToDirectExchange()
         {
             using (IConnection conn = _factory.CreateConnection())
             using (IModel channel = conn.CreateModel())
             {
-                channel.QueueDeclare(_q1, true, false, false, null);
-                channel.QueueDeclare(_q2, true, false, false, null);
-                channel.ExchangeDeclare(_exchange,"direct");
-                channel.QueueBind(_q1,_exchange,_routingKey);
-                channel.QueueBind(_q2,_exchange,_routingKey);
+                channel.QueueDeclare(_directQueue, true, false, false, null);
+                channel.ExchangeDeclare(_directExchange, "direct");
+                channel.QueueBind(_directQueue, _directExchange, _routingKey);
 
-                const string message = "Message sent through direct exchange type";
+                const string message = "Hello Motor Team!!  This message is sent through a direct exchange.";
                 var encodedMessage = System.Text.Encoding.Unicode.GetBytes(message);
-                channel.BasicPublish(_exchange,_routingKey,null,encodedMessage);
+                channel.BasicPublish(_directExchange, _routingKey, null, encodedMessage);
             }
-        }
-
-        public string ReceiveFromDirect()
-        {
-            string message1, message2;
-
-            using (IConnection conn = _factory.CreateConnection())
-            using (IModel channel = conn.CreateModel())
-            {
-                //channel.QueueDeclare(_q1, true, false, false, null);
-                //channel.QueueDeclare(_q2, true, false, false, null);
-                //channel.ExchangeDeclare(_exchange, "direct");
-                //channel.QueueBind(_q1, _exchange, _routingKey);
-                //channel.QueueBind(_q2, _exchange, _routingKey);
-
-                var encodedMessage1 = channel.BasicGet(_q1, false);
-                var encodedMessage2 = channel.BasicGet(_q2, false);
-
-                message1 = System.Text.Encoding.Unicode.GetString(encodedMessage1.Body);
-                message2 = System.Text.Encoding.Unicode.GetString(encodedMessage2.Body);
-
-            }
-
-            return message1 + message2;
         }
     }
-    
-    class Program
+
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var rabbitMq = new RabbitMQ();
-            rabbitMq.SendToFanOut();
-            var receiveFromFanOut = rabbitMq.ReceiveFromFanOut();
-            Console.Write("Message received from Fan Out Exchange: {0}\n\n", receiveFromFanOut);
-            
-            rabbitMq.SendToDirect();
-            var receiveFromDirect = rabbitMq.ReceiveFromDirect();
-            Console.Write("Message received from Direct Exchange: {0}\n\n", receiveFromDirect);
-            Console.WriteLine("Press any key to finish");
-            Console.Read();
+            var publish = new MessagingPublish();
+
+            Console.WriteLine("");
+            Console.WriteLine("Main Menu (Send Messages)");
+            Console.WriteLine("==========================");
+            Console.WriteLine("1. Send Fan Out Exchange Message");
+            Console.WriteLine("2. Send Direct Exchange Message");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine("Enter your choice: ");
+            var input = Console.ReadLine();
+            int choice = 0;
+
+            if (!int.TryParse(input, out choice)) return;
+            switch (choice)
+            {
+                case 1:
+                    publish.SendToFanOutExchange();
+                    Console.Write("Message sent to Fan Out Exchange");
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    publish.SendToDirectExchange();
+                    Console.Write("Message received from Direct Exchange");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Environment.Exit(0);
+                    break;
+            }
         }
     }
 }
